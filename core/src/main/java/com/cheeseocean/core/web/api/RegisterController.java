3d6d17e1439e66e3649c0f67fdeca40ba3058b38
xxxcrel <xxxcrel@gmail.com>
2025-06-11 10:22:44 +0800

    Feat: add login service

--- core/src/main/java/com/cheeseocean/core/web/api/RegisterController.java
+++ core/src/main/java/com/cheeseocean/core/web/api/RegisterController.java
@@ -1,52 +1,58 @@
 package com.cheeseocean.core.web.api;
 
+import com.cheeseocean.common.util.VerifyCodeGenerator;
+import com.cheeseocean.common.web.response.Result;
+import com.cheeseocean.core.api.UserService;
+import com.cheeseocean.core.service.MailService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.constraints.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
 public class RegisterController {
 
-//    public final Log logger = LogFactory.getLog(getClass());
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @Autowired
-//    private MailService mailService;
-//
-//    @PostMapping(value = "/check", consumes = MediaType.TEXT_PLAIN_VALUE)
-//    public void check(@RequestBody String s) throws IOException {
-////        System.out.println(request.getInputStream().read());
-//        System.out.println("check");
-//        System.out.println(s);
-//    }
-//
-//    @GetMapping(value = "/check", params = "username")
-//    @ResponseStatus(HttpStatus.OK)
-//    public void usernameCheck(@RequestParam("username")String username){
-//        if(userService.presentByUsername(username))
-//            throw new AlreadyExistsException("user:" + username + " already exists");
-//    }
-//
-//    @GetMapping(value = "/code/email", params = "address")
-//    @ResponseStatus(HttpStatus.OK)
-//    public Result<String> verifyEmail(@RequestParam("address")@Email String email, Model model, HttpSession session){
-//        if(userService.presentByEmail(email))
-//            throw new AlreadyExistsException("email already registered");
-//
-//        String verifyCode = VerifyCodeGenerator.generate(6);
-//        model.addAttribute("verifyDTO", new UserRegisterDTO.VerifyDTO(verifyCode, LocalDateTime.now()));
-//        mailService.sendMail(email, verifyCode);
-//
-//        return Result.ok("verify email already send to: " + email);
-//    }
-//
-//    @PostMapping("/register")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public Result<String> register(@Valid UserRegisterDTO registerDTO,
-//                                   @SessionAttribute(value = "verifyDTO") UserRegisterDTO.VerifyDTO verifyDTO,
-//                                   SessionStatus sessionStatus) {
-//        if(!verifyDTO.check(registerDTO.getCode(), LocalDateTime.now()))
-//            return new Result<>("Time out, please resend email verification");
-//        sessionStatus.setComplete();
-//        logger.info("register dto: " + registerDTO);
-//        userService.register(registerDTO);
-//        return Result.ok("register successful");
-//    }
+    public final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private MailService mailService;
+
+    @PostMapping(value = "/check", consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void check(@RequestBody String s) throws IOException {
+//        System.out.println(request.getInputStream().read());
+        System.out.println("check");
+        System.out.println(s);
+    }
+
+    @GetMapping(value = "/check", params = "username")
+    @ResponseStatus(HttpStatus.OK)
+    public void usernameCheck(@RequestParam("username") String username) {
+
+    }
+
+    @GetMapping(value = "/code/email", params = "address")
+    @ResponseStatus(HttpStatus.OK)
+    public Result<String> verifyEmail(@RequestParam("address") @Email String email, Model model, HttpSession session) {
+
+        String verifyCode = VerifyCodeGenerator.generate(6);
+        mailService.sendMail(email, verifyCode);
+
+        return Result.ok("verify email already send to: " + email);
+    }
+
 }
